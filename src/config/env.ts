a41d5a0beeb46e01a42674b069494fbca09360c1
apawar4 <akshay.pawar@morningstar.com>
2026-06-14 09:01:17 +0530

    Changes in ENV FIle

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,13 +1,20 @@
 import * as dotenv from 'dotenv';
+import path from 'path';
+import fs from 'fs';
 
-const environment = process.env.TTA_ENV || 'qa';
+const environment = process.env.TEST_ENV  || 'qa';
 
-dotenv.config({
-    path: `./config/${environment}.env`
-});
+const envFilePath = path.resolve(__dirname, `${environment}.env`);
+
+if (!fs.existsSync(envFilePath)) {
+    throw new Error(`Environment file not found: ${envFilePath}`);
+}
+
+dotenv.config({ path: envFilePath });
+console.log(`[env] Selected environment: ${environment}`);
 
 export const env = {
     baseURL: process.env.APP_URL || 'https://app.thetestingacademy.com/playwright/ttacart/',
     username: process.env.APP_USERNAME || 'standard_user',
-    password: process.env.APP_PASSWORD || 'tta_secret', 
-} 
+    password: process.env.APP_PASSWORD || 'tta_secret',
+};
